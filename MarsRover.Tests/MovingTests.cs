d19a922d215239ec3127f535b6e6b64e71d29a1f
mehdi okhovat <okhovvat.mehdi@gmail.com>
2022-11-09 00:09:11 +0330

    added moves towards west in one direction

--- MarsRover.Tests/MovingTests.cs
+++ MarsRover.Tests/MovingTests.cs
@@ -5,26 +5,39 @@ namespace MarsRover.Tests;
 public class MovingTests
 {
     [Fact]
-    public void Rover_moves_by_command()
+    public void moves_one_direction_towards_north()
     {
         var rover = new Rover();
 
-        string roverPosition =  rover.Execute("MMMM");
+        string roverPosition = rover.Execute("MMMM");
 
         roverPosition.Should().Be("4:0:N");
     }
+
+    [Fact]
+    public void moves_one_direction_towards_west()
+    {
+        var rover = new Rover();
+
+        var position = rover.Execute("RMM");
+
+        position.Should().Be("0:2:W");
+    }
+    
+    
 }
 
 public class Rover
 {
+    
     public string Execute(string s)
     {
+        var substringLenght = s.Substring(0, s.Length - 1).Length;
+        if (s.Substring(0,1) == "R")
+        {
+            return $"0:{substringLenght}:W";
+        }
         var counter = s.Length;
         return $"{counter}:0:N";
-                
-        if (s.Equals("MM"))
-            return "2:0:N";
-        
-        return "1:0:N";
     }
 }
