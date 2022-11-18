46ec15bb991582beeb290b5a2d252a6e65e12e0c
mehdi okhovat <okhovvat.mehdi@gmail.com>
2022-11-18 22:19:43 +0330

    added 2 times turn to right.

--- MarsRovers.Tests/MarsRoverShould.cs
+++ MarsRovers.Tests/MarsRoverShould.cs
@@ -5,11 +5,13 @@ namespace MarsRovers.Tests;
 
 public class MarsRoverShould
 {
-    [Fact]
-    public void turn_right()
+    [Theory]
+    [InlineData("R","0:0:E")]
+    [InlineData("RR","0:0:S")]
+    public void turn_right(string command, string position)
     {
         var marsRover = new MarsRover2();
         
-        marsRover.execute().Should().Be("0:0:E");
+        marsRover.Execute(command).Should().Be(position);
     }
 }
--- codeKatas/MarsRover2.cs
+++ codeKatas/MarsRover2.cs
@@ -2,8 +2,21 @@ namespace codeKatas;
 
 public class MarsRover2
 {
-    public string execute()
+    private string _direction = "N";
+
+    public string Execute(string command)
     {
-        return "0:0:E";
+        foreach (var item in command.ToCharArray())
+        {
+            if (_direction.Equals("N"))
+                _direction = "E";
+            else if (_direction.Equals("E"))
+            {
+                _direction = "S";
+            }
+        }
+
+
+        return $"0:0:{_direction}";
     }
 }
