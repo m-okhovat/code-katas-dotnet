32bfe817c42ee54fafc4b33c1d8dc5cbe21543b3
mehdi okhovat <okhovvat.mehdi@gmail.com>
2022-11-21 22:19:11 +0330

    extracted duplicated variables in tests

--- MarsRovers.Tests/MarsRoverShould.cs
+++ MarsRovers.Tests/MarsRoverShould.cs
@@ -5,6 +5,13 @@ namespace MarsRovers.Tests;
 
 public class MarsRoverShould
 {
+    private MarsRover2 _marsRover;
+
+    public MarsRoverShould()
+    {
+        _marsRover = new MarsRover2();
+    }
+
     [Theory]
     [InlineData("R", "0:0:E")]
     [InlineData("RR", "0:0:S")]
@@ -12,28 +19,31 @@ public class MarsRoverShould
     [InlineData("RRRR", "0:0:N")]
     public void turn_right(string command, string position)
     {
-        var marsRover = new MarsRover2();
-
-        marsRover.Execute(command).Should().Be(position);
+        var roverPosition = _marsRover.Execute(command);
+        
+        roverPosition.Should().Be(position);
     }
 
     [Theory]
-    [InlineData("L","0:0:W")]
-    [InlineData("LL","0:0:S")]
-    [InlineData("LLL","0:0:E")]
-    [InlineData("LLLL","0:0:N")]
+    [InlineData("L", "0:0:W")]
+    [InlineData("LL", "0:0:S")]
+    [InlineData("LLL", "0:0:E")]
+    [InlineData("LLLL", "0:0:N")]
     public void turn_left(string command, string position)
     {
-        var marsRover = new MarsRover2();
-
-        marsRover.Execute(command).Should().Be(position);
+        var roverPosition = _marsRover.Execute(command);
+        
+        roverPosition.Should().Be(position);
     }
 
     [Fact]
     public void moves_to_north()
     {
-        MarsRover2 marsRover = new MarsRover2();
-        
-        marsRover.Execute("M").Should().Be("0:1:N");
+        const string expected = "0:1:N";
+        const string command = "M";
+
+        var roverPosition = _marsRover.Execute(command);
+
+        roverPosition.Should().Be(expected);
     }
 }
