663fd8ceb8568b23493b0742f3a4b09a2d9f5566
mehdi okhovat <okhovvat.mehdi@gmail.com>
2022-11-27 00:14:14 +0330

    added roman convertors up to 100

--- RomanNumbers.Tests/RomanNumeralConvertorShould.cs
+++ RomanNumbers.Tests/RomanNumeralConvertorShould.cs
@@ -10,6 +10,23 @@ public class RomanNumeralConvertorShould
     [InlineData(2, "II" )]
     [InlineData(3, "III" )]
     [InlineData(4, "IV" )]
+    [InlineData(5, "V" )]
+    [InlineData(6, "VI" )]
+    [InlineData(7, "VII" )]
+    [InlineData(8, "VIII" )]
+    [InlineData(9, "IX" )]
+    [InlineData(10, "X" )]
+    [InlineData(20, "XX" )]
+    [InlineData(30, "XXX" )]
+    [InlineData(32, "XXXII" )]
+    [InlineData(40, "XL" )]
+    [InlineData(50, "L" )]
+    [InlineData(60, "LX" )]
+    [InlineData(70, "LXX" )]
+    [InlineData(80, "LXXX" )]
+    [InlineData(90, "XC" )]
+    [InlineData(95, "XCV" )]
+    [InlineData(100, "C" )]
     public void convert_one(int number , string expectedRomanNumber)
     {
         var romanNumeralConvertor = new RomanNumeralConvertor();
--- codeKatas/RomanNumbers/RomanNumeralConvertor.cs
+++ codeKatas/RomanNumbers/RomanNumeralConvertor.cs
@@ -4,12 +4,50 @@ public class RomanNumeralConvertor
 {
     public string Convert(int number)
     {
-        if (number < 4)
-        {
-            var romans = new string[] {"I", "II","III"};
-            return romans[number-1];    
-        }
+        if (RomanDigitStore.HasKey(number)) 
+            return  RomanDigitStore.GetByKey(number);
 
-        return "IV";
+        if (number <4)
+        return Convert(number - 1) + RomanDigitStore.GetByKey(1);
+
+        if(number<10)
+        return RomanDigitStore.GetByKey(5) + Convert(number - 5);
+
+        if (number < 50)
+        return RomanDigitStore.GetByKey(10) + Convert(number - 10);
+
+        if(number < 90)
+        return RomanDigitStore.GetByKey(50) + Convert(number - 50);
+
+        return RomanDigitStore.GetByKey(90) + Convert(number - 90);
+
+    }
+
+}
+public static class  RomanDigitStore
+{
+    private static readonly Dictionary<int, string> _collection  = new Dictionary<int, string>()
+    {
+        {1, "I"},
+        {4, "IV"},
+        {5, "V"},
+        {9, "IX"},
+        {10, "X"},
+        {40, "XL"},
+        {50, "L"},
+        {90, "XC"},
+        {100, "C"},
+    };
+
+    public static string GetByKey(int number)
+    {
+        _collection.TryGetValue(number, out var result);
+
+        return result;
+    }
+
+    public static bool HasKey(int number)
+    {
+        return _collection.ContainsKey(number);
     }
 }
