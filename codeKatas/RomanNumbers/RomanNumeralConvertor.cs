09b8de1c34b1d97a1d30afe15eeb59e15aaf8e49
mehdi okhovat <okhovvat.mehdi@gmail.com>
2022-12-01 12:54:29 +0330

    added new steps

--- codeKatas/RomanNumbers/RomanNumeralConvertor.cs
+++ codeKatas/RomanNumbers/RomanNumeralConvertor.cs
@@ -4,29 +4,66 @@ public class RomanNumeralConvertor
 {
     public string Convert(int number)
     {
-        if (RomanDigitStore.HasKey(number)) 
-            return  RomanDigitStore.GetByKey(number);
+        var numbers = new Dictionary<int, string>()
+        {
+            {1, "I"},
+            {2, "II"},
+            {3, "III"},
+            {4, "IV"},
+            {5, "V"},
+            {9, "IX"},
+            {10, "X"},
+        };
 
-        if (number <4)
-        return Convert(number - 1) + RomanDigitStore.GetByKey(1);
+        var result = string.Empty;
 
-        if(number<10)
-        return RomanDigitStore.GetByKey(5) + Convert(number - 5);
+        if (numbers.ContainsKey(number))
+        {
+            result = getValue(number, numbers);
 
-        if (number < 50)
-        return RomanDigitStore.GetByKey(10) + Convert(number - 10);
+            return result;
+        }
+        
+        if (number > 10)
+            return getValue(10, numbers) + Convert(number - 10);
 
-        if(number < 90)
-        return RomanDigitStore.GetByKey(50) + Convert(number - 50);
+        if (number < 9)
+            return getValue(5, numbers) + Convert(number - 5);
 
-        return RomanDigitStore.GetByKey(90) + Convert(number - 90);
-
+        return string.Empty;
+        // if (RomanDigitStore.HasKey(number)) 
+        //     return  RomanDigitStore.GetByKey(number);
+        //
+        //
+        //
+        //
+        // while (number < 4)
+        // {
+        //     return Convert(number-1) + RomanDigitStore.GetByKey(1);
+        // }
+        //
+        // if(number<10)
+        // return RomanDigitStore.GetByKey(5) + Convert(number - 5);
+        //
+        // if (number < 50)
+        // return RomanDigitStore.GetByKey(10) + Convert(number - 10);
+        //
+        // if(number < 90)
+        // return RomanDigitStore.GetByKey(50) + Convert(number - 50);
+        //
+        // return RomanDigitStore.GetByKey(90) + Convert(number - 90);
     }
 
+    private static string? getValue(int number, Dictionary<int, string> numbers)
+    {
+        numbers.TryGetValue(number, out var result);
+        return result;
+    }
 }
-public static class  RomanDigitStore
+
+public static class RomanDigitStore
 {
-    private static readonly Dictionary<int, string> _collection  = new Dictionary<int, string>()
+    private static readonly Dictionary<int, string> _collection = new Dictionary<int, string>()
     {
         {1, "I"},
         {4, "IV"},
