3a7cecc8216c2b9f3d6b82e243630320ca5bcafc
mehdi okhovat <okhovvat.mehdi@gmail.com>
2022-12-01 19:56:42 +0330

    removed duplicated while loops

--- codeKatas/RomanNumbers/RomanNumeralConvertor.cs
+++ codeKatas/RomanNumbers/RomanNumeralConvertor.cs
@@ -2,63 +2,71 @@ namespace codeKatas.RomanNumbers;
 
 public class RomanNumeralConvertor
 {
-    private Dictionary<int, string> _numbers =  new Dictionary<int, string>()
+    private Dictionary<int, string> _numbers = new Dictionary<int, string>()
     {
-        {1, "I"},
-        {4, "IV"},
-        {5, "V"},
-        {9, "IX"},
-        {10, "X"},
-        {40, "XL"},
         {50, "L"},
+        {40, "XL"},
+        {10, "X"},
+        {9, "IX"},
+        {5, "V"},
+        {4, "IV"},
+        {1, "I"}
     };
 
     public string Convert(int number)
     {
-      
         var result = string.Empty;
-       
-        while (number >= 50)
-        {
-            number -= 50;
-            result += getValue(50);
-        }
 
-        while (number >= 40)
+        foreach (var key in _numbers.Keys)
         {
-            number -= 40;
-            result += getValue(40);
+            while (number >= key)
+            {
+                number -= key;
+                result += getValue(key);
+            }
         }
-
-        while (number >= 10)
-        {
-            number -= 10;
-            result += getValue(10);
-        }
-
-        while (number >= 9)
-        {
-            number -= 9;
-            result += getValue(9);
-        }
-
-        while (number >= 5)
-        {
-            number -= 5;
-            result += getValue(5);
-        }
-
-        while (number >= 4)
-        {
-            number -= 4;
-            result += getValue(4);
-        }
-
-        while (number >= 1)
-        {
-            number -= 1;
-            result += getValue(1);
-        }
+        //
+        // while (number >= 50)
+        // {
+        //     number -= 50;
+        //     result += getValue(50);
+        // }
+        //
+        // while (number >= 40)
+        // {
+        //     number -= 40;
+        //     result += getValue(40);
+        // }
+        //
+        // while (number >= 10)
+        // {
+        //     number -= 10;
+        //     result += getValue(10);
+        // }
+        //
+        // while (number >= 9)
+        // {
+        //     number -= 9;
+        //     result += getValue(9);
+        // }
+        //
+        // while (number >= 5)
+        // {
+        //     number -= 5;
+        //     result += getValue(5);
+        // }
+        //
+        // while (number >= 4)
+        // {
+        //     number -= 4;
+        //     result += getValue(4);
+        // }
+        //
+        // while (number >= 1)
+        // {
+        //     number -= 1;
+        //     result += getValue(1);
+        // }
 
         return result;
     }
